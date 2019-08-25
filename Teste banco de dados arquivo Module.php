35b3c37b30e439256e6bfd754d6bc12e2f87395b
João Bonfim <joaojudo182@gmail.com>
2019-08-25 17:32:51 -0300

    ajuste

---
+++ Teste banco de dados arquivo Module.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application;
+
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\Adapter\Driver\ResultInterface;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\EventManager\EventInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+
+class Module implements BootstrapListenerInterface
+{
+    const VERSION = '3.0.3-dev';
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+
+    public function onBootstrap(EventInterface $e)
+    {
+        /**
+         * @var $serviceManager \Zend\ServiceManager\ServiceManager
+         * @var $adapter Adapter
+         */
+        $serviceManager = $e->getApplication()->getServiceManager();
+
+        $adapter = $serviceManager->get(Adapter::class);
+
+        $statement = $adapter->createStatement('SELECT * FROM users');
+        $statement->prepare();
+        $result = $statement->execute();
+
+        if ($result instanceof ResultInterface && $result->isQueryResult()) {
+            $resultSet = new ResultSet;
+            $resultSet->initialize($result);
+
+            foreach ($resultSet as $row) {
+                echo $row->id . PHP_EOL;
+                echo $row->name . PHP_EOL;
+                echo $row->email . PHP_EOL;
+            }
+        }
+
+        die;
+    }
+}
